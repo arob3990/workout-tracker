83a6781c7d028db659b3eabee98b7451925afa0c
Alex Robertson <alex.robertson390@gmail.com>
2023-04-17 21:40:31 -0700

    uri update

--- client/src/App.js
+++ client/src/App.js
@@ -26,9 +26,8 @@ import './App.css';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'mongodb+srv://arobertson:temppassword@cluster0.k3fp0ms.mongodb.net/?retryWrites=true&w=majority',
+  uri: '/graphql',
 });
-
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
