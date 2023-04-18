780905268b7c3baf68147d7d6ea6b88e3bb89270
Alex Robertson <alex.robertson390@gmail.com>
2023-04-17 19:09:20 -0700

    env variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/workout-tracker',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
